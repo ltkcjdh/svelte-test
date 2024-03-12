7080e62217812fa05172a36df3af5a9c71124a4e
ltkcjdh <150325592+ltkcjdh@users.noreply.github.com>
2024-03-12 13:17:14 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Имя Фамилия</title>
+
+</svelte:head>
+<div class="container">
+    <div class="info">
+        <div class="animate-background">
+            <div class="container info-container">
+                <div class="info-text">
+                    <h1>
+                        <p class="animate-text">Lorem ipsum dolor sit amet.</p>
+                        <p class="twxt-2">Lorem, ipsum dolor.</p>
+
+                    </h1>
+                </div>
+                <h2 class="subtitle">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni, nisi!
+                </h2>
+                <div style='color: black'class="scroll-button animate-scroll" tabindex="0">
+                    <a href="/about">Click Here</a>
+                    <div class="arrow">---</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="background-div"></div>
+
+
 <style>
     .info{
         text-align: center;
